687422130e7ec85c597e15815900ab84e5dad08c
gimigh <mydovicythelp@gmail.com>
2023-03-11 16:38:26 +0200

    ver1

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ class Cat:
         self.energy = 100
 
     def eat(self):
-        self.hunger = max(0, self.hunger - 20)
-        self.energy = min(100, self.energy + 20)
-        print(f"{self.name} is eating,please wait")
+        self.hunger -= 30
+        self.energy += 10
+        print(f"{self.name} is eating")
 
     def play(self):
-        self.energy = max(0, self.energy - 30)
-        self.hunger = min(100, self.hunger + 10)
-        self.thirst = min(100, self.thirst + 10)
+        self.energy -= 30
+        self.hunger += 10
+        self.mood += 40
         print(f"{self.name} is playing")
+
+    def sleep(self):
+        self.hunger += 10
+        self.mood -= 5
+        self.energy +=50
+        print(f"{self.name} is sleeping")
+
+        if self.hunger >= 10 or self.happiness <= 0:
+            self.is_alive = False
+
+    def __str__(self):
+        return f"{self.name}: mood {self.mood}, hunger {self.hunger}, energy {self.energy}"
+
+cat = Cat("myay")
